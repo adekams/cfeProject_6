9ca7ad7b98b78c9891f325a55b8a5ce90420ae93
cutebid <67562053+cutebid@users.noreply.github.com>
2020-11-24 22:08:48 +0100

    schoolmangement

---
+++ script.js
@@ -0,0 +1,48 @@
+// Please check for your names, thats the method you will be working with....thanks    
+ 
+
+let School = {
+                    name: " King's College",
+                     address: " Lagos ",
+                     student: [],
+                      staff: [],
+
+
+       // Adenike              
+        getAllStudents: function(){},
+        getStudent: function(id){},
+
+        //  Balikisu
+        modifyStudent: function(id){},
+        registerStudent: function(name, age, sex, ){},
+
+        //  Asiyah
+        employStaff: function(name, gender, subject, yearsOfExperience, qualification, contact){},
+        getAllStaffs: function(){},
+
+        // Samuel
+        getStaffId: function(id){},
+        modifyStaff: function(id){},
+
+        // Aminat animashaun , please use a splice method for the delete
+        deleteStaff: function(id){},
+
+        
+        // faith
+        getStudentByClass: function(){},
+        deleteStudent: function(id){},
+        
+   
+        // Sunday
+        SortStudentByName: function(){},
+
+        // michael
+        getSchoolAccount: function(){},
+        getSchoolInfo: function(){},
+    
+ };      
+    
+    
+    
+    
+   
